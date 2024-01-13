13caf3d1875d013cade3c95b2f3e95f3801b6295
Siva-Tejaa <asivateja1999@gmail.com>
2024-01-13 18:20:16 +0530

    Routes Updated in node and express js code

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use("/file", file);
 
 app.use("*", error);
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
